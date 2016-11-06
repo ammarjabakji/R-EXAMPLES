2b8312a13a1d872241198f17529ea637b18244b7
Ammar Jabakji <ammarjabakji@gmail.com>
2016-11-06 18:26:59 +0300

    I've added a new R project

---
+++ first.R
@@ -0,0 +1,5 @@
+# Create a numeric vector containing the numbers 
+# 2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23. 
+# What is the average of these numbers?
+x = c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+mean(x)
