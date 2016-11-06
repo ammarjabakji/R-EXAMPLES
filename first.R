5fc6205d5469fc8d8eef684c225a865fbc515a39
Ammar Jabakji <ammarjabakji@gmail.com>
2016-11-06 19:07:40 +0300

    change the numbers

--- first.R
+++ first.R
@@ -1,5 +1,5 @@
-# Create a numeric vector containing the numbers 
-# 2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23. 
+# Create a vector containing the follwing numbers 
+# 42, 66, 16, 33, 13, 18, 43. 
 # What is the average of these numbers?
-x = c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+x = c(42, 66, 16, 33, 13, 18, 43)
 mean(x)
